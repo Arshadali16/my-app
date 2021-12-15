2ab06fa41b7dbfb419eb77919c20521cc42cd457
Arshadali16 <arshadalishahani16@gmail.com>
2021-12-15 06:17:21 -0800

    my

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class About extends Component {
+
+    render(){
+        return(
+
+            <div className="container">
+                <h1>About Component</h1>
+            </div>
+
+        );
+    }
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+ const App = () => {
+  return (
+
+    <div className="App">
+      <h2>Hello Jsx!</h2>
+    </div>
+
+  );
+};
+
+export default App;
